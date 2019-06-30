11b1e2690520ba3c828820c13e1df06246ea23fe
Coo van Leeuwen <info@cerem.co>
2019-06-30 15:41:42 +0200

    added docker files

--- model/GitHelper.php
+++ model/GitHelper.php
@@ -10,15 +10,15 @@ class GitHelper {
 	 *
 	 * Runs 'git describe'
 	 *
-	 * @return string
+	 * @return string|null
 	 */
-	public static function gitVersion(): string
+	public static function gitVersion()
 	{
 		exec('git describe --always',$version);
 		return $version[0];
 	}
 
-	public static function gitDateString(): string
+	public static function gitDateString()
 	{
 		exec('git log --pretty="%ci" -n1 HEAD', $date);
 		return $date[0];
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Log Viewer 2 API
+
+## Docker
+
+The docker image's entrypoint is under `/app/public/index.php`. Settings should be placed at `/app/settings.json`.
+
+Docker compose example can be found in the front-end repo.
